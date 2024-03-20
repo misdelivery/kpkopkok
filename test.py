2fa800c7eee3bdbb9f576f8035d63e34c956eac5
misdelivery <130632231+misdelivery@users.noreply.github.com>
2024-03-21 07:57:08 +0900

    b

--- test.py
+++ test.py
@@ -8,20 +8,21 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.bfloat16,
     device_map="auto",
 )
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 prompt = "<s>[INST]まど☆マギで一番かわいいのは誰ですか？[/INST]"
-input_ids = tokenizer.encode(
-    prompt,
-    add_special_tokens=False,
-    return_tensors="pt"
-)
-tokens = model.generate(
-    input_ids.to(device=model.device),
-    max_new_tokens=128,
-    temperature=0.99,
-    top_p=0.95,
-    do_sample=True,
+
+generator_params = dict(
+    max_new_tokens = 256,
+    do_sample = True,
+    temperature = 0.99,
+    top_p = 0.95,
+    pad_token_id = tokenizer.eos_token_id,
 )
 
-out = tokenizer.decode(tokens[0], skip_special_tokens=True)
-print(out)
+output = generator(
+    prompt,
+    **generator_params,
+)
+
+print(output[0]["generated_text"])
