4adcbcd97eda589b545aeab8b1a92f32a49e8703
misdelivery <130632231+misdelivery@users.noreply.github.com>
2024-03-21 02:01:00 +0900

    add

---
+++ inference.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, MambaForCausalLM, MambaConfig
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained('mambabyte-130m_checkpoints/checkpoint-13000')
+if tokenizer.pad_token_id is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+mamba_config = MambaConfig(
+    hidden_size = 768,
+    num_hidden_layers = 24,
+    vocab_size = len(tokenizer),
+    pad_token_id=tokenizer.pad_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    use_cache=False
+)
+
+model = MambaForCausalLM.from_pretrained('mambabyte-130m_checkpoints/checkpoint-13000', config=mamba_config)
+
+import torch
+model.to('cuda:0')
+with torch.no_grad():
+    input_ids = tokenizer.encode("夏目漱石は、", add_special_tokens=False, return_tensors="pt")
+    output_ids = model.generate(
+    input_ids.to(model.device),
+    max_length=800,
+)
+
+print(tokenizer.decode(output_ids.tolist()[0], skip_special_tokens=True))
+
+with torch.no_grad():
+    input_ids = tokenizer.encode("夏目漱石は、", add_special_tokens=False, return_tensors="pt")
+    output_ids = model.generate(
+    input_ids.to(model.device),
+    max_length=800,
+    do_sample=True,
+    temperature=0.1,
+    repetition_penalty=1.1
+)
+
+print(tokenizer.decode(output_ids.tolist()[0], skip_special_tokens=True))
+
